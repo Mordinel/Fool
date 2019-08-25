2c560639d5978faf27744cd23ce81473b28a94c4
Mason <sorom2@student.op.ac.nz>
2019-08-26 00:33:50 +1200

    Fixed truncated strncpy.

--- fool.c
+++ fool.c
@@ -42,8 +42,8 @@ int main(int argc, char *argv[]) {
     out_file = fopen(argv[2], "w");
     free(out_file);
 
-    magic_bytes = (char *)malloc(sizeof(char)*6);
-    strncpy(magic_bytes, "\x47\x49\x46\x38\x39\x61", 6);
+    magic_bytes = (char *)malloc(sizeof(char)*7);
+    strncpy(magic_bytes, "\x47\x49\x46\x38\x39\x61", 7);
 
     write_bytes(&magic_bytes, &in_file, &out_file);
 
