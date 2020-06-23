3d045fff6f02d4d094281776a95fc0d38cb8a48e
Mason <sorom2@student.op.ac.nz>
2020-06-24 02:57:03 +1200

    Add main exit code variation, call the destructor, make error more verbose.

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,21 @@
 int main(int argc, char* argv[]) 
 {
     Fool* fool;
+    int exit_code = 0;
+
     if (argc != 3) {
-        fprintf(stderr, "Usage:\n\t%s <in file> <out file.extension>\n", argv[0]);
+        fprintf(stderr, "Usage:\n%s <in file> <out file.extension>\n", argv[0]);
         return 1;
     }
 
     fool = new Fool(argv[1], argv[2]);
 
     if(!fool->WriteFile()) {
-        fprintf(stderr, "Failed\n");
-        return 1;
+        fprintf(stderr, "Could not complete action. Fix your permissions or use a supported file extension.\n");
+        exit_code = 1;
     }
 
-    return 0;
+    delete fool;
+
+    return exit_code;
 }
